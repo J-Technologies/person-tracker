1ea7b47f8cf05febfcb5324649eaa8ca1b4578c4
Teije van Sloten <teije.van.sloten@hva.nl>
2016-04-29 18:01:03 +0200

    Made some simpler enums

---
+++ src/main/scala/nl/ordina/personen/datatypes/package.scala
@@ -0,0 +1,26 @@
+package nl.ordina.personen
+
+package object datatypes {
+
+  sealed abstract class Geslachtsaanduiding(val code: String, val naam: String){}
+
+  case object MAN extends Geslachtsaanduiding("M", "Man")
+  case object VROUW extends Geslachtsaanduiding("V", "Vrouw")
+  case object ONBEKEND extends Geslachtsaanduiding("O", "Onbekend")
+
+  case class Burgerservicenummer(value: String) {
+    private val cijfers = value.map(c => c.asDigit)
+    assert(cijfers.length == 9, "Een burgerservicenummer bestaat uit negen cijfers")
+    assert(elfproef, s"$value voldoet niet aan de elfproef")
+
+    private def elfproef = (
+      cijfers
+        .slice(0, 8)
+        .zipWithIndex
+        .map { case (cijfer, index) => (9 - index) * cijfer }
+        .sum - cijfers(8)
+      ) % 11 == 0
+
+    override def toString: String = value
+  }
+}
--- src/test/scala/nl/ordina/personen/datatypes/GeslachtsaanduidingTest.scala
+++ src/test/scala/nl/ordina/personen/datatypes/GeslachtsaanduidingTest.scala
@@ -1,26 +1,22 @@
 package nl.ordina.personen.datatypes
 
-import nl.ordina.personen.datatypes.Geslachtsaanduiding.GeslachtsaanduidingWaarde
 import org.scalatest.{FunSuite, Matchers}
 
 class GeslachtsaanduidingTest extends FunSuite with Matchers {
 
   test("test VROUW") {
-    val vrouw: GeslachtsaanduidingWaarde = Geslachtsaanduiding.VROUW
-    vrouw.code shouldBe "V"
-    vrouw.naam shouldBe "Vrouw"
+    VROUW.code shouldBe "V"
+    VROUW.naam shouldBe "Vrouw"
   }
 
   test("test ONBEKEND") {
-    val onbekend: GeslachtsaanduidingWaarde = Geslachtsaanduiding.ONBEKEND
-    onbekend.code shouldBe "O"
-    onbekend.naam shouldBe "Onbekend"
+    ONBEKEND.code shouldBe "O"
+    ONBEKEND.naam shouldBe "Onbekend"
   }
 
   test("test MAN") {
-    val man: GeslachtsaanduidingWaarde = Geslachtsaanduiding.MAN
-    man.code shouldBe "M"
-    man.naam shouldBe "Man"
+    MAN.code shouldBe "M"
+    MAN.naam shouldBe "Man"
   }
 
 }
