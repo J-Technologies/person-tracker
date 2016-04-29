96779e414b829245b2da04d25547256cfe102992
Eric Jan Malotaux <Ericjan.Malotaux@kadaster.nl>
2016-04-29 23:53:13 +0200

    Zo kort mogelijk.

--- src/main/scala/nl/ordina/personen/datatypes/package.scala
+++ src/main/scala/nl/ordina/personen/datatypes/package.scala
@@ -9,8 +9,10 @@ package object datatypes {
 
   case class Burgerservicenummer(value: String) {
     assert(value.length == 9, "Een burgerservicenummer heeft negen cijfers")
-    private val nummer = value.map(c => c.asDigit)
-    private val elfproef = (nummer.slice(0, 8).zipWithIndex.map(e => (9 - e._2) * e._1).sum - nummer(8)) % 11 == 0
+    private val cijfers = value.map(c => c.asDigit)
+    private val controlecijfer = cijfers(8)
+    private val som = cijfers.take(8).zipWithIndex.map(e => (9 - e._2) * e._1).sum
+    private val elfproef = controlecijfer == som % 11
     assert(elfproef, s"$value is geen geldig burgerservicenummer")
   }
   object Burgerservicenummer {
