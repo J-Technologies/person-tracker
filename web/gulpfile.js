f1680ba7520a2d3d3d50a9f33eff576221ca766f
Teije van Sloten <teije.van.sloten@hva.nl>
2016-05-27 17:52:39 +0200

    Added browser reload and sass compile tasks

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,5 +1,29 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function () {
+    // place code for your default task here
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
 });
