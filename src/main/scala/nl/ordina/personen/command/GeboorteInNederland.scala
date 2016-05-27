d16b9e43643df920002f6f1e0dd59eb3d4c642fd
Eric Jan Malotaux <Ericjan.Malotaux@kadaster.nl>
2016-05-27 15:47:54 +0200

    Geslachtsaanduiding toegevoegd.

--- src/main/scala/nl/ordina/personen/command/GeboorteInNederland.scala
+++ src/main/scala/nl/ordina/personen/command/GeboorteInNederland.scala
@@ -4,6 +4,7 @@ import nl.ordina.personen.datatype._
 
 case class GeboorteInNederland(
   naamEnNaamgebruik: SamengesteldeNaam,
+  geslacht: Geslachtsaanduiding = null,
   geboorte: Geboorte,
   bijhoudingspartij: Partij
 )
--- src/test/scala/nl/ordina/personen/command/GeboorteInNederlandTest.scala
+++ src/test/scala/nl/ordina/personen/command/GeboorteInNederlandTest.scala
@@ -1,5 +1,6 @@
 package nl.ordina.personen.command
 
+import nl.ordina.personen.datatype.Geslachtsaanduiding.MAN
 import nl.ordina.personen.datatype._
 import org.scalatest.{FunSuite, Matchers}
 
@@ -8,10 +9,12 @@ class GeboorteInNederlandTest extends FunSuite with Matchers {
   test("Creëer een 'GeboorteInNederland'") {
     val geboorte = GeboorteInNederland(
       SamengesteldeNaam(voornamen = Voornamen("Dirk"), geslachtsnaamstam = Geslachtsnaamstam("Luijk")),
-      Geboorte(Datum("1993-01-01"), Gemeente("0505")), Partij("000505")
+      MAN,
+      geboorte = Geboorte(Datum("1993-01-01"), Gemeente("0505")), bijhoudingspartij = Partij("000505")
     )
     geboorte.naamEnNaamgebruik.voornamen.toString should be("Dirk")
     geboorte.naamEnNaamgebruik.achternaam should be("Luijk")
+    geboorte.geslacht should be(MAN)
     geboorte.bijhoudingspartij.naam should be(Partijnaam("Gemeente Dordrecht"))
     geboorte.geboorte.gemeente.naam.value should be("Dordrecht")
     geboorte.geboorte.datum.toString should be("1993-01-01")
