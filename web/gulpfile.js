e87d1b881a873758adfefa56fe9dfe95ad90c1bb
Teije van Sloten <teije.van.sloten@hva.nl>
2016-06-10 10:11:05 +0200

    Basic gulp tasks implemented

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -6,17 +6,29 @@ gulp.task('default', ['watch'], function () {
     // place code for your default task here
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function () {
+gulp.task('watch', ['browserSync', 'sass', 'copy-js', 'copy-index-html'], function () {
     gulp.watch('src/scss/**/*.scss', ['sass']);
 
-    gulp.watch('src/*.html', browserSync.reload);
-    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('src/*.html', ['copy-index-html']);
+    gulp.watch('src/js/**/*.js', ['copy-js']);
 });
 
 gulp.task('sass', function () {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('copy-index-html', function() {
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('copy-js', function() {
+    gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.reload({stream: true}))
 });
 
