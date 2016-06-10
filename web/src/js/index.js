c741f34db0d8a54439ceca800f44aa8149067380
Teije van Sloten <teije.van.sloten@hva.nl>
2016-06-10 11:35:52 +0200

    React implemented

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1,6 +1,13 @@
-var socket = new WebSocket('ws://localhost:8123/todos/websocket');
+import ReactDOM from 'react-dom'
+import React from 'react'
 
-socket.onmessage = function (msg) {
+let socket = new WebSocket('ws://localhost:8123/todos/websocket');
+socket.onmessage = (msg) => {
     document.getElementsByTagName('pre')[0].innerHTML += msg.data + '\n';
     console.log(msg.data);
 };
+
+ReactDOM.render(
+    <h1>I should not think</h1>,
+    document.getElementById('example')
+);
