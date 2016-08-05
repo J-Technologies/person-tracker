315932237ecf703f75642324bf5f838015c7c6d0
leeuwg <Gerard.deLeeuw@kadaster.nl>
2016-08-05 16:18:52 +0200

    Fix compile error

--- soap/src/main/scala/nl/ordina/personen/handlers/soap/SoapEventHandler.scala
+++ soap/src/main/scala/nl/ordina/personen/handlers/soap/SoapEventHandler.scala
@@ -1,27 +1,6 @@
 package nl.ordina.personen.handlers.soap
 
-import nl.ordina.personen.event
-import nl.ordina.personen.event.{PersoonGeboren, PersoonOverleden}
-import org.axonframework.eventhandling.{EventHandler, SimpleEventHandlerInvoker, TrackingEventProcessor}
-
-import scala.collection.mutable
-
 class SoapEventHandler {
-
-  val eventProcessor = new TrackingEventProcessor("soap", new SimpleEventHandlerInvoker(this), event.eventStore, event.tokenStore)
-  val queue = new mutable.Queue[String]()
-
-  eventProcessor.start()
-
-  @EventHandler
-  def handle(event: PersoonGeboren) {
-    queue += s"PersoonGeboren: ${event.bsn} $event"
-  }
-
-  @EventHandler
-  def handle(event: PersoonOverleden) {
-    queue += s"PersoonOverleden: ${event.bsn}"
-  }
 }
 
 object SoapEventHandler {
