fa10a7c268063cae1f0de221bbf397f1abfcf0f7
Teije van Sloten <teije.van.sloten@hva.nl>
2016-08-05 15:54:25 +0200

    Removed useless header stuffs

--- web/src/js/HeaderNav.js
+++ web/src/js/HeaderNav.js
@@ -6,18 +6,16 @@ export default ({title, children}) => {
         <nav className="navbar navbar-inverse navbar-fixed-top">
             <div className="container">
                 <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
+
                     <a className="navbar-brand" href="#">{title}</a>
+
                 </div>
                 <div id="navbar" className="collapse navbar-collapse">
+
                     <ul className="nav navbar-nav">
                         {children}
                     </ul>
+
                 </div>
             </div>
         </nav>
