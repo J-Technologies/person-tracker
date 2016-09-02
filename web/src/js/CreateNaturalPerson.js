f20b438752c4a4678d3083059a1a8374dd6aa32f
Teije van Sloten <teije.van.sloten@hva.nl>
2016-09-02 16:12:13 +0200

    Added Lodash, added ouders fields, can have up-to two values

--- web/src/js/CreateNaturalPerson.js
+++ web/src/js/CreateNaturalPerson.js
@@ -1,42 +1,81 @@
-import React from "react";
+import React, {Component} from "react";
 import InputField from "./form/InputField";
 import RadioButtonField from "./form/RadioButtonField";
 import SubmitButton from "./form/SubmitButton";
-import service from "./service/PersonService"
+import service from "./service/PersonService";
+import _ from "lodash";
 
-export default () => {
+export default class PersonDeceased extends Component {
 
-    const handleSubmit = e => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ouders: 0
+        }
+    }
+
+    handleSubmit(e) {
         e.preventDefault();
         service.createNaturalPerson(new FormData(document.getElementById('createPerson')));
     };
 
-    return (
-        <div>
-            <h1>Schrijf een natuurlijk persoon in</h1>
-            <hr/>
-            <form className="form-horizontal" id="createPerson" onSubmit={handleSubmit}>
+    addOuder(e) {
+        e.preventDefault();
+        const {ouders} = this.state;
+        if (ouders < 2) {
+            this.setState({ouders: ouders + 1})
+        }
+    };
 
-                <InputField id="voornaam" label="Voornaam"/>
+    render() {
+        const {ouders} = this.state;
 
-                <InputField id="achternaam" label="Achternaam"/>
+        return (
+            <div>
+                <h1>Schrijf een natuurlijk persoon in</h1>
+                <hr/>
+                <form className="form-horizontal" id="createPerson" onSubmit={this.handleSubmit}>
 
-                <RadioButtonField name="geslacht"
-                                  label="Gelacht"
-                                  buttons={[
-                                    {id: 'onbekend', label: 'Onbekend'},
-                                    {id: 'vrouw', label: 'Vrouw'},
-                                    {id: 'man', label: 'Man'}
-                                  ]}/>
+                    <InputField id="voornaam" label="Voornaam"/>
 
-                <InputField id="geboortedatum" label="Geboortedatum" type="date"/>
+                    <InputField id="achternaam" label="Achternaam"/>
 
-                <input name="gemeente" type="hidden" value="0505"/>
-                <input name="partij" type="hidden" value="000505"/>
-                
-                <SubmitButton />
-            </form>
-        </div>
-    )
+                    <RadioButtonField name="geslacht"
+                                      label="Gelacht"
+                                      buttons={[
+                                          {id: 'onbekend', label: 'Onbekend'},
+                                          {id: 'vrouw', label: 'Vrouw'},
+                                          {id: 'man', label: 'Man'}
+                                      ]}/>
 
+                    <InputField id="geboortedatum" label="Geboortedatum" type="date"/>
+
+                    <input name="gemeente" type="hidden" value="0505"/>
+                    <input name="partij" type="hidden" value="000505"/>
+
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between'
+
+                    }}>
+                        <h3>Ouders</h3>
+                        <button className="btn btn-success"
+                                disabled={ouders >= 2}
+                                style={{alignSelf: 'center'}}
+                                type="button"
+                                onClick={this.addOuder.bind(this)}>
+                            add
+                        </button>
+                    </div>
+
+                    <hr/>
+
+                    {_.range(ouders).map(i => <InputField key={i} id="ouder" label="Ouder bsn"/>)}
+
+                    <SubmitButton />
+                </form>
+            </div>
+        )
+    }
 };
